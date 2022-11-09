0aba41595b9ccabe761182574aa4a6c82de989ef
ebanri <emrebanri@gmail.com>
2022-11-09 22:44:02 +0300

    if-else örnekleri

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace if_else{
+
+    class Program{
+        static void Main(string[] args){
+            int saat = DateTime.Now.Hour;
+
+            if(saat <=18 && saat >= 12{
+                Console.WriteLine("İyi Günler!");
+            }
+            if(saat <=12 && saat >= 6{
+                Console.WriteLine("Günaydın!");
+            }
+            if(saat <=23 && saat >= 18{
+                Console.WriteLine("İyi Akşamlar");
+            }
+            if(saat <=23 && saat >= 6{
+                Console.WriteLine("İyi Geceler");
+            }
+        }
+    }
+}
